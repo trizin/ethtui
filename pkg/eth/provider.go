544a76f68c17a3d8121f80ee94c77307c02613d2
trizin <25263018+trizin@users.noreply.github.com>
2022-10-08 20:44:37 +0000

    Add "GetEthValue" func

--- pkg/eth/provider.go
+++ pkg/eth/provider.go
@@ -40,3 +40,10 @@ func (p Provider) GetBalance(address string, blockNumber int64) *big.Int {
 	return bal
 }
 
+func GetEthValue(wei *big.Int) float64 {
+	fbalance := new(big.Float)
+	fbalance.SetString(wei.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	val, _ := ethValue.Float64()
+	return val
+}
