2107eca7cdf3df6dcc7736d272a4462bc2cf61cf
trizin <25263018+trizin@users.noreply.github.com>
2022-10-09 11:40:42 +0000

    Add test `TestWalletData_SignTransaction`

---
+++ pkg/eth/eth_test.go
@@ -0,0 +1,22 @@
+package eth
+
+import (
+	"testing"
+)
+
+func TestWalletData_SignTransaction(t *testing.T) {
+	wallet := GenerateWallet()
+	signed := wallet.SignTransaction(
+		0,
+		"0x000000000000000000000000000000000000dEaD",
+		10,
+		10000,
+		100,
+		"0x",
+		1,
+	)
+
+	if signed == "" {
+		t.Errorf("WalletData.SignTransaction() = %v", signed)
+	}
+}
